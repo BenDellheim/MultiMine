7a18bdf553d6d07477a5f8326383e0cec020dbda
Ben <dellheimb15@students.ecu.edu>
2016-12-06 22:10:02 -0500

    Added multithread code to MServer. Problem 1 finished.

---
+++ src/minesweeper/server/MinerRunnable.java
@@ -0,0 +1,83 @@
+package minesweeper.server;
+import java.net.*;
+import java.io.*;
+
+/* Use: MinesweeperServer starts a new Thread for each connected user, which calls run().
+ * Code modeled from an example on Jenkov.com:
+ * http://tutorials.jenkov.com/java-multithreaded-servers/multithreaded-server.html
+ */
+public class MinerRunnable implements Runnable {
+	protected Socket socket = null;
+
+    public MinerRunnable(Socket s) {
+        this.socket = s;
+    }
+
+    public void run() {
+        
+        try {
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            
+            out.println("Welcome to Minesweeper. ?? people are playing including you.");
+            out.println("Type 'help' for help.");
+
+        	for (String line = in.readLine(); line != null; line = in.readLine()) {
+        		String output = handleRequest(line);
+        		if(output != null) {
+        			out.println(output);
+        		}
+        	}
+        	out.close();
+        	in.close();
+        } catch (IOException e) {
+            //report exception somewhere.
+            e.printStackTrace();
+        } 
+    }
+
+	/**
+	 * handler for client input
+	 * 
+	 * make requested mutations on game state if applicable, then return appropriate message to the user
+	 * 
+	 * @param input
+	 * @return
+	 */
+	private static String handleRequest(String input) {
+
+		String regex = "(look)|(dig \\d+ \\d+)|(flag \\d+ \\d+)|(deflag \\d+ \\d+)|(help)|(bye)";
+		if(!input.matches(regex)) {
+			//invalid input
+			return null;
+		}
+		String[] tokens = input.split(" ");
+		if(tokens[0].equals("look")) {
+			// 'look' request
+			//TODO Question 5
+		} else if(tokens[0].equals("help")) {
+			// 'help' request
+			//TODO Question 5
+		} else if(tokens[0].equals("bye")) {
+			// 'bye' request
+			//TODO Question 5
+		} else {
+			int x = Integer.parseInt(tokens[1]);
+			int y = Integer.parseInt(tokens[2]);
+			if(tokens[0].equals("dig")) {
+				// 'dig x y' request
+				//TODO Question 5
+			} else if(tokens[0].equals("flag")) {
+				// 'flag x y' request
+				//TODO Question 5
+			} else if(tokens[0].equals("deflag")) {
+				// 'deflag x y' request
+				//TODO Question 5
+			}
+		}
+		//should never get here
+		return "";
+	}
+    
+
+}
--- src/minesweeper/server/MinesweeperServer.java
+++ src/minesweeper/server/MinesweeperServer.java
@@ -5,6 +5,7 @@ import java.io.*;
 
 public class MinesweeperServer {
 
+	private boolean isStopped = false;
 	private final static int PORT = 4444;
 	private ServerSocket serverSocket;
 	
@@ -13,7 +14,7 @@ public class MinesweeperServer {
      * @param port port number, requires 0 <= port <= 65535.
      */
     public MinesweeperServer(int port) throws IOException {
-        serverSocket = new ServerSocket(port);
+        this.serverSocket = new ServerSocket(port);
     }
     
     /**
@@ -23,97 +24,45 @@ public class MinesweeperServer {
      * (IOExceptions from individual clients do *not* terminate serve()).
      */
     public void serve() throws IOException {
-        while (true) {
-            // block until a client connects
-            Socket socket = serverSocket.accept();
-            
-            // handle the client
+        while (!isStopped()) {
+            Socket socket = null;
             try {
-                handleConnection(socket);
+                // block until a client connects
+                socket = serverSocket.accept();
             } catch (IOException e) {
                 e.printStackTrace(); // but don't terminate serve()
-            } finally {
-                socket.close();
             }
+            // Start the client thread and listen for more connections
+            new Thread(new MinerRunnable(socket)).start();
         }
     }
     
-    /**
-     * Handle a single client connection.  Returns when client disconnects.
-     * @param socket  socket where client is connected
-     * @throws IOException if connection has an error or terminates unexpectedly
-     */
-    private void handleConnection(Socket socket) throws IOException {
-        
-        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+    private synchronized boolean isStopped() {
+        return this.isStopped;
+    }
 
+
+    public synchronized void stop(){
+        this.isStopped = true;
         try {
-        	for (String line = in.readLine(); line != null; line = in.readLine()) {
-        		String output = handleRequest(line);
-        		if(output != null) {
-        			out.println(output);
-        		}
-        	}
-        } finally {        
-        	out.close();
-        	in.close();
+            this.serverSocket.close();
+        } catch (IOException e) {
+            throw new RuntimeException("Error closing server", e);
         }
     }
 
-	/**
-	 * handler for client input
-	 * 
-	 * make requested mutations on game state if applicable, then return appropriate message to the user
-	 * 
-	 * @param input
-	 * @return
-	 */
-	private static String handleRequest(String input) {
-
-		String regex = "(look)|(dig \\d+ \\d+)|(flag \\d+ \\d+)|(deflag \\d+ \\d+)|(help)|(bye)";
-		if(!input.matches(regex)) {
-			//invalid input
-			return null;
-		}
-		String[] tokens = input.split(" ");
-		if(tokens[0].equals("look")) {
-			// 'look' request
-			//TODO Question 5
-		} else if(tokens[0].equals("help")) {
-			// 'help' request
-			//TODO Question 5
-		} else if(tokens[0].equals("bye")) {
-			// 'bye' request
-			//TODO Question 5
-		} else {
-			int x = Integer.parseInt(tokens[1]);
-			int y = Integer.parseInt(tokens[2]);
-			if(tokens[0].equals("dig")) {
-				// 'dig x y' request
-				//TODO Question 5
-			} else if(tokens[0].equals("flag")) {
-				// 'flag x y' request
-				//TODO Question 5
-			} else if(tokens[0].equals("deflag")) {
-				// 'deflag x y' request
-				//TODO Question 5
-			}
-		}
-		//should never get here
-		return "";
-	}
-    
     /**
      * Start a MinesweeperServer running on the default port.
      */
     public static void main(String[] args) {
+    	MinesweeperServer server = null;
         try {
-            MinesweeperServer server = new MinesweeperServer(PORT);
+            server = new MinesweeperServer(PORT);
             server.serve();
         } catch (IOException e) {
             e.printStackTrace();
         }
+        server.stop();
     }
 
 }
